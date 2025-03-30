e89d8fb31a455e293e0fdc3f61a6852c4e75792a
ThisUserTaken <hirunadf@gmail.com>
2025-03-30 15:10:27 -0700

    latest working pathplanner with new tunerconstants

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -1,3 +1,4 @@
+
 package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.*;
@@ -9,12 +10,13 @@ import com.ctre.phoenix6.Utils;
 import com.ctre.phoenix6.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.swerve.SwerveRequest;
-import com.ctre.phoenix6.hardware.Pigeon2;
+import com.ctre.phoenix6.hardware.Pigeon2;  
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.config.RobotConfig;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+
 
 import edu.wpi.first.math.Matrix;
 import edu.wpi.first.math.VecBuilder;
@@ -25,18 +27,18 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-
 import frc.robot.LimelightHelpers;
-import frc.robot.generated.TunerConstants;
 import frc.robot.generated.TunerConstants.TunerSwerveDrivetrain;
+import frc.robot.generated.TunerConstants;
+
 
 /**
  * Class that extends the Phoenix 6 SwerveDrivetrain class and implements
@@ -61,26 +63,6 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
     private final SwerveRequest.SysIdSwerveTranslation m_translationCharacterization = new SwerveRequest.SysIdSwerveTranslation();
     private final SwerveRequest.SysIdSwerveSteerGains m_steerCharacterization = new SwerveRequest.SysIdSwerveSteerGains();
     private final SwerveRequest.SysIdSwerveRotation m_rotationCharacterization = new SwerveRequest.SysIdSwerveRotation();
-
-    /* Gyro for field-centric control */
-    private Pigeon2 m_gyro = getPigeon2();
-
-    
-    
-    private SwerveDrivePoseEstimator m_poseEstimator = new SwerveDrivePoseEstimator(
-        getKinematics(), // swerve drive kinematics
-        kBlueAlliancePerspectiveRotation,  // current gyro angle
-        new SwerveModulePosition[] // swerve module pose
-            {
-                getModule(0).getPosition(true),
-                getModule(1).getPosition(true), 
-                getModule(2).getPosition(true), 
-                getModule(3).getPosition(true)
-            },
-        new Pose2d(), // init pose
-        VecBuilder.fill(0.05,0.05,Units.degreesToRadians(5)), // odometry errors
-        VecBuilder.fill(0.5,0.5,Units.degreesToRadians(30))); // april tag errors
-    
 
     /* SysId routine for characterizing translation. This is used to find PID gains for the drive motors. */
     private final SysIdRoutine m_sysIdRoutineTranslation = new SysIdRoutine(
@@ -143,8 +125,23 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
 
     /* The SysId routine to test */
     private SysIdRoutine m_sysIdRoutineToApply = m_sysIdRoutineTranslation;
+    private SwerveDrivePoseEstimator m_poseEstimator = new SwerveDrivePoseEstimator(
+        getKinematics(), // swerve drive kinematics
+        kBlueAlliancePerspectiveRotation,  // current gyro angle
+        new SwerveModulePosition[] // swerve module pose
+            {
+                getModule(0).getPosition(true),
+                getModule(1).getPosition(true), 
+                getModule(2).getPosition(true), 
+                getModule(3).getPosition(true)
+            },
+        new Pose2d(), // init pose
+        VecBuilder.fill(0.05,0.05,Units.degreesToRadians(5)), // odometry errors
+        VecBuilder.fill(0.5,0.5,Units.degreesToRadians(30))); // april tag errors
+                                                                                    
 
-    
+    private Pigeon2 m_gyro = getPigeon2();
+
 
     /**
      * Constructs a CTRE SwerveDrivetrain using the specified constants.
@@ -161,7 +158,6 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
         SwerveModuleConstants<?, ?, ?>... modules
     ) {
         super(drivetrainConstants, modules);
-        m_gyro = new Pigeon2(TunerConstants.kPigeonId); // Initialize the Pigeon2 gyro
         if (Utils.isSimulation()) {
             startSimThread();
         }
@@ -187,7 +183,6 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
         SwerveModuleConstants<?, ?, ?>... modules
     ) {
         super(drivetrainConstants, odometryUpdateFrequency, modules);
-        m_gyro = new Pigeon2(TunerConstants.kPigeonId); // Initialize the Pigeon2 gyro
         if (Utils.isSimulation()) {
             startSimThread();
         }
@@ -222,18 +217,18 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
         SwerveModuleConstants<?, ?, ?>... modules
     ) {
         super(drivetrainConstants, odometryUpdateFrequency, odometryStandardDeviation, visionStandardDeviation, modules);
-        m_gyro = new Pigeon2(TunerConstants.kPigeonId); // Initialize the Pigeon2 gyro
         if (Utils.isSimulation()) {
             startSimThread();
         }
         configureAutoBuilder();
     }
-    
+
     private void configureAutoBuilder() {
         try {
             var config = RobotConfig.fromGUISettings();
             AutoBuilder.configure(
-                () -> m_poseEstimator.getEstimatedPosition(),   // Supplier of current robot pose
+                () -> getState().Pose,
+                // () -> m_poseEstimator.getEstimatedPosition(),   // Supplier of current robot pose
                 this::resetPose,         // Consumer for seeding pose against auto
                 () -> getState().Speeds, // Supplier of current robot speeds
                 // Consumer of ChassisSpeeds and feedforwards to drive the robot
@@ -290,70 +285,11 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
         return m_sysIdRoutineToApply.dynamic(direction);
     }
 
-
-
-
-    public void updateOdometry() {
-        m_poseEstimator.update(
-            kBlueAlliancePerspectiveRotation,  // current gyro angle
-            new SwerveModulePosition[] // swerve module pose
-            {
-                getModule(0).getPosition(true),
-                getModule(1).getPosition(true), 
-                getModule(2).getPosition(true), 
-                getModule(3).getPosition(true)
-            }
-        );
-
-        boolean useMegaTag2 = true; //set to false to use MegaTag1
-        boolean doRejectUpdate = false;
-        if (useMegaTag2 == false) {
-            LimelightHelpers.PoseEstimate mt1 = LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight-threeg");
-            
-            if (mt1.tagCount == 1 && mt1.rawFiducials.length == 1) {
-                if(mt1.rawFiducials[0].ambiguity > .7) {
-                doRejectUpdate = true;
-                }
-                if(mt1.rawFiducials[0].distToCamera > 3) {
-                doRejectUpdate = true;
-                }
-            }
-            if (mt1.tagCount == 0) {
-                doRejectUpdate = true;
-            }
-
-            if (!doRejectUpdate) {
-                m_poseEstimator.setVisionMeasurementStdDevs(VecBuilder.fill(.5,.5,9999999));
-                m_poseEstimator.addVisionMeasurement(
-                    mt1.pose,
-                    mt1.timestampSeconds);
-            }
-        } else if (useMegaTag2 == true) {
-            LimelightHelpers.SetRobotOrientation("limelight-threeg", m_poseEstimator.getEstimatedPosition().getRotation().getDegrees(), 0, 0, 0, 0, 0);
-            LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight-threeg");
-            if (Math.abs(m_gyro.getRate()) > 720) {// if our angular velocity is greater than 720 degrees per second, ignore vision updates
-                doRejectUpdate = true;
-            }
-            if (mt2.tagCount == 0) {
-                doRejectUpdate = true;
-            }
-            if (!doRejectUpdate) {
-                double[] stddevs = NetworkTableInstance.getDefault().getTable("limelight-threeg").getEntry("stddevs").getDoubleArray(new double[]{0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7, 0.7});
-                // The 7th element (index 6) is the x stddev and the 8th element (index 7) is the y stddev
-                double xStdDev = stddevs.length > 6 ? stddevs[6] : 0.7;
-                double yStdDev = stddevs.length > 7 ? stddevs[7] : 0.7;
-                m_poseEstimator.setVisionMeasurementStdDevs(VecBuilder.fill(xStdDev,yStdDev,9999999));
-                m_poseEstimator.addVisionMeasurement(
-                    mt2.pose,
-                    mt2.timestampSeconds); 
-            }
-        }
-    }
-
-
-
     @Override
     public void periodic() {
+        System.out.println("Pose Estimator: " + m_poseEstimator);
+        // System.out.println("Gyro: " + m_gyro);
+        // System.out.println("Pose Estimated Position " + m_poseEstimator.getEstimatedPosition());
         /*
          * Periodically try to apply the operator perspective.
          * If we haven't applied the operator perspective before, then we should apply it regardless of DS state.
@@ -361,6 +297,11 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
          * Otherwise, only check and apply the operator perspective if the DS is disabled.
          * This ensures driving behavior doesn't change until an explicit disable event occurs during testing.
          */
+
+
+
+
+
         if (!m_hasAppliedOperatorPerspective || DriverStation.isDisabled()) {
             DriverStation.getAlliance().ifPresent(allianceColor -> {
                 setOperatorPerspectiveForward(
@@ -370,11 +311,29 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
                 );
                 m_hasAppliedOperatorPerspective = true;
             });
-        }
+        }   
+        // Integrate Limelight vision updates
+        // LimelightHelpers.SetRobotOrientation("limelight", m_poseEstimator.getEstimatedPosition().getRotation().getDegrees(), 0, 0, 0, 0, 0);
+        // LimelightHelpers.PoseEstimate mt2 = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2("limelight");
+        // boolean doRejectUpdate = false;
+        // if (Math.abs(m_gyro.getRate()) > 720) // if our angular velocity is greater than 720 degrees per second, ignore vision updates
+        // {
+        //     doRejectUpdate = true;
+        // }
+        // if (mt2.tagCount == 0)
+        // {
+        //     doRejectUpdate = true;
+        // }
+        // if (!doRejectUpdate)
+        // {
+        //     m_poseEstimator.setVisionMeasurementStdDevs(VecBuilder.fill(.7, .7, 9999999));
+        //     m_poseEstimator.addVisionMeasurement(
+        //         mt2.pose,
+        //         mt2.timestampSeconds);
+        // }
 
-        updateOdometry();
+
     }
-    
 
     private void startSimThread() {
         m_lastSimTime = Utils.getCurrentTimeSeconds();
